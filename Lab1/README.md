13e21dc7d880df6c113846b184c6e60f7560a1a9
AdamSzr <adam.szr98@gmail.com>
2020-11-10 21:10:45 +0100

    film 4_06_43

--- Lab1/README.md
+++ Lab1/README.md
@@ -14,8 +14,8 @@
 
 ![Usuwanie](md_img/delete.png)
 ***
-## Lista postów przed ( usunięciem postu nr. 3).
-![EfektPrzed](md_img/post_3.png)
+## Lista postów przed ( usunięciem postu nr. 6).
+![EfektPrzed](md_img/przed_post_6.png)
 ## Lista postów po.
-![EfektPrzed](md_img/post_3_del.png)
+![EfektPrzed](md_img/po_post_6.png)
 
--- Lab1/mysite/settings.py
+++ Lab1/mysite/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
+from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -119,5 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+django_heroku.settings(locals())
