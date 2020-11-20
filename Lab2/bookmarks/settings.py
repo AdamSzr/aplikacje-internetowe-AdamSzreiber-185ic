e0b50f39c6b4c2904c557ac88b0923b20ac96589
AdamSzr <44463475+AdamSzr@users.noreply.github.com>
2020-11-20 08:51:58 +0100

    Update settings.py

--- Lab2/bookmarks/settings.py
+++ Lab2/bookmarks/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +129,4 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+django_heroku.settings(locals())
